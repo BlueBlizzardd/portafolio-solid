55a9ace4a3ee8d69d27af6c3312860f7b134788d
BlueBlizzardd <105523256+BlueBlizzardd@users.noreply.github.com>
2024-12-28 01:33:59 -0500

    added an option to toggle animations, TODO: fix any breaking changes

--- src/routes/(menus)/options.tsx
+++ src/routes/(menus)/options.tsx
@@ -5,18 +5,12 @@ import { MenuItem } from '../../components/menu-item';
 
 export const Options: Component = () => {
     const options: string[] = ['Toggle Animations'];
-    let speed: 0 | 0.5 = 0;
-
-    const animationToggle = () => {
-        document.documentElement.style.setProperty('--animation-speed', `${speed}`);
-        speed = speed ? 0 : 0.5;
-    }
 
     return (
         <div class="d-grid options">
             <Index each={options}>
                 {option =>
-                    <MenuItem onclick={animationToggle} title={option()} />
+                    <MenuItem title={option()} />
                 }
             </Index>
             <A href='/'>
